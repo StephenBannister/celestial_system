e15c491bf12afc8bf034cfaa7b16a41bca32c057
MordrigeSHU <c4060968@hallam.shu.ac.uk>
2024-11-26 21:04:05 +0000

    - added system_menu selection tests, including Mock to isolate the code base under test

--- tests.py
+++ tests.py
@@ -2,11 +2,19 @@
     - Sheffield Hallam University, 2024. Micro-Lecture - Error Handling. [online] SHUspace. Available at: https://shuspace.shu.ac.uk/ultra/courses/_351506_1/outline/edit/document/_13967799_1?courseId=_351506_1&view=content.
     - Stack Overflow, 2024. Explain the setUp() and tearDown() Python methods used in test cases. [online] Available at: https://stackoverflow.com/questions/6854658/explain-the-setup-and-teardown-python-methods-used-in-test-cases
     - Python Discussion Forum, 2024. Deleting an object. [online] Available at: https://discuss.python.org/t/deleting-an-object/17299/2
+    - Python Software Foundation, 2024. unittest.mock.Mock — Mock class. [online] Available at: https://docs.python.org/3/library/unittest.mock.html#unittest.mock.Mock
 """
 
+''' Git commit notes:
+
+
+
+'''
 
 import unittest
+from unittest.mock import Mock
 from celestial import CelestialBody, Star, Planet, Moon
+from system_menu import SystemMenu
 
 
 class CelestialSystemTest(unittest.TestCase):
@@ -86,6 +94,60 @@ class CelestialSystemTest(unittest.TestCase):
         self.assertEqual(celestial_body.get_orbiting_objects(), [], "An empty orbiting objects property causes an error")
         self.assertEqual(celestial_body.get_orbiting_object_names(), "None", "An empty orbiting objects names property causes an error")
         
+
+class MenuSystemTest(unittest.TestCase):
+    def setUp(self) -> None:
+        self.mock_solar_system = Mock()
+        self.mock_solar_system.get_orbiting_object_names.return_value = "Earth, Mars, Venus, Jupiter"
+        self.menu = SystemMenu(self.mock_solar_system)
+        
+        
+    def tearDown(self) -> None:
+        self.mock_solar_system = None
+        self.menu = None
+        
+    
+  # ---------------- Menu Choice NLP and Logic ------------------  
+    
+    def test_check_menu_choice_valid_sentence_is_all_planet_info(self):
+        choice, planet_choice = self.menu.determine_menu_choice("tell me about Mars")
+        self.assertEqual (choice, 1)
+        self.assertEqual (planet_choice, "Mars")
+
+    def test_check_menu_choice_valid_word_is_all_planet_info(self):
+        choice, planet_choice = self.menu.determine_menu_choice("venus")
+        self.assertEqual (choice, 1)
+        self.assertEqual (planet_choice, "Venus")
+
+    def test_check_menu_choice_valid_sentence_is_planet_mass(self):
+        choice, planet_choice = self.menu.determine_menu_choice("What is the mass of mars")
+        self.assertEqual (choice, 2)
+        self.assertEqual (planet_choice, "Mars")
+
+    def test_check_menu_choice_valid_sentence_is_planet_exists(self):
+        choice, planet_choice = self.menu.determine_menu_choice("does earth exist")
+        self.assertEqual (choice, 3)
+        self.assertEqual (planet_choice, "Earth")
+
+    def test_check_menu_choice_valid_sentence_how_many_moons(self):
+        choice, planet_choice = self.menu.determine_menu_choice("how many moons does earth have")
+        self.assertEqual (choice, 4)
+        self.assertEqual (planet_choice, "Earth")
+
+    def test_check_menu_choice_valid_sentence_show_all(self):
+        choice, planet_choice = self.menu.determine_menu_choice("tell me everything")
+        self.assertEqual (choice, 5)
+    
+    def test_check_menu_choice_valid_non_matched_input(self):
+        choice, planet_choice = self.menu.determine_menu_choice("this is unmatched")
+        self.assertIsNone(planet_choice, None)
+        self.assertIsNone(choice, None)
+        
+
+
+    
+    
+
 
 if __name__ == "__main__":
     unittest.main()
